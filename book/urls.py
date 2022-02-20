f2e778befbe1d1d569ce0d2198aa97a19d8cc8ce
unknown <you@example.com>
2022-02-20 14:36:03 +0600

    hw3

--- book/urls.py
+++ book/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('book_list/', views.book_list, name='books'),
     path('book_list/<int:id>/', views.book_detail, name='booksdetail'),
+    path('add_books_list/', views.create_book, name='add book'),
 ]
 
 
--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
+from  django.http import HttpResponse
 from django.shortcuts import render
-from . import models
+from . import models,forms
 from django.shortcuts import render, get_object_or_404
 
 def book_list(request):
@@ -9,3 +10,14 @@ def book_list(request):
 def book_detail(request, id):
     books = get_object_or_404(models.Book, id=id)
     return render(request, 'book_detail.html', {'books': books})
+
+def create_book(request):
+    method = request.method
+    if method == 'POST':
+        form = forms.BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('book created')
+    else:
+        form = forms.BookForm()
+        return render(request, 'add_book.html', {'form':form})
